9f3a072dec58d9efe26cb8f18a986255f5043517
M M Danish <danishsadpara@gmail.com>
2024-05-30 21:56:24 +0500

    added express and typescript demo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,16 @@
-const sayHello = (name: string): string => {
-  return `Hello, ${name}!`;
-};
+import express from "express";
+import dotenv from "dotenv";
 
-console.log(sayHello("World"));
+// load environment variables
+dotenv.config({ path: ".env.local" });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send("Hello Developers");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,14 @@
 export default function Home() {
+  type optional<Type> = {
+    [Property in keyof Type]+?: Type[Property];
+  };
+  type user = {
+    name: string;
+    email: string;
+    password: string;
+    age: string;
+  };
+  type mutableUser = optional<user>;
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Danish</p>
